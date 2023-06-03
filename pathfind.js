b279ac1b89a4a6f8c2ade176574289783443f693
son <gudtn2238@gmail.com>
2023-06-04 02:16:37 +0900

    0604작업

--- pathfind.js
+++ pathfind.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 exports.searchPlace = async function(req, res) {
     const { value } = req.params;
     var arr = [];
@@ -19,7 +21,7 @@ exports.searchPlace = async function(req, res) {
                 "x" : data.documents[i].x,
                 "y" : data.documents[i].y
              };
-             arr.push(sendData)
+             arr.push(sendData);
         }
     } else { // 주소 검색결과가 없으면 키워드 검색
         url = `https://dapi.kakao.com/v2/local/search/keyword.json?query=${value}`;
@@ -32,9 +34,178 @@ exports.searchPlace = async function(req, res) {
                 "x" : data.documents[i].x,
                 "y" : data.documents[i].y
              };
-             arr.push(sendData)
+             arr.push(sendData);
         }
     }
     console.log(arr);
-    res.json(arr)
+    res.json(arr);
+}
+
+exports.pathFind = async function(req, res) {
+    const { value } = req.params;
+    const val = JSON.parse(value);
+    const sx = val[0][2][1];
+    const sy = val[0][3][1];
+    const ex = val[1][2][1];
+    const ey = val[1][3][1];
+    const odsayKey = '0QNZgti0UA7t0YRwd3T7Qs2pyfFuFAHK6ZrPCSV/KS4'; // odsay api키
+    const tmapKey = 'XkfHq8f9ff9te9zmfe3Y28d3DehpIIQd1FQnA8kL'; // tmap api키
+    const odsayUrl = `https://api.odsay.com/v1/api/searchPubTransPathT?apiKey=${odsayKey}&lang=0&SX=${sx}&SY=${sy}&EX=${ex}&EY=${ey}`; // odsay url
+    const tmapUrl = 'https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&callback=function'; // tmap url
+    let options = { // tmap api 옵션
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            appKey: tmapKey
+        },
+        body: JSON.stringify({
+            startX: 0,
+            startY: 0,
+            endX: 0,
+            endY: 0,
+            startName: '%ec%8b%9c%ec%9e%91',
+            endName: '%eb%81%9d',
+            searchOption: '0',
+        })
+    };
+
+    const odRes = await fetch(odsayUrl);
+    const odData = await odRes.json();
+    const path = odData.result.path;
+    console.log('=======================================');
+
+    const pathArr = [];
+    const pathArr2 = [];
+    for (let i = 0; i < path.length; i++) {
+        const pathSec = [];
+        const pathE = path[i];
+        let needWalk = false;
+        let totalTime = 0;
+        let tmpB = JSON.parse(options.body);
+        tmpB.startX = Number(sx);
+        tmpB.startY = Number(sy);
+        options.body = JSON.stringify(tmpB);
+        for (let j = 0; j < pathE.subPath.length; j++) {
+            const subE = pathE.subPath[j];
+            // trafficType ==> 1:지하철, 2:버스, 3:도보, 4:지하철 환승, 5:버스 환승
+            if(subE.trafficType == 3 && subE.distance == 0) { // 환승일때
+                if(pathE.subPath[j-1].trafficType == 1) { // 지하철 - 지하철 환승
+                    subE.trafficType = 4;
+                    subE.sectionTime = [pathE.subPath[j-1].lane[0].subwayCode, pathE.subPath[j+1].lane[0].subwayCode];
+                } else { // 버스-버스 제자리 환승
+                    subE.trafficType = 5;
+                }
+                var tmpE = {
+                    trafficType: subE.trafficType,
+                    sectionTime: subE.sectionTime
+                }
+                pathSec.push(tmpE)
+            }
+            if(subE.trafficType == 3) { // 도보
+                needWalk = true;
+                if(j == pathE.subPath.length - 1) { // 마지막 도보
+                    let tmpB = JSON.parse(options.body);
+                    tmpB.endX = Number(ex);
+                    tmpB.endY = Number(ey);
+                    options.body = JSON.stringify(tmpB);
+                    const tRes = await fetch(tmapUrl, options);
+                    const tData = await tRes.json();
+                    const tDistance = tData.features[0].properties.totalDistance;
+                    const tTime = calculateTime(tDistance);
+                    
+                    var tmpE = {
+                        trafficType: subE.trafficType,
+                        startX: tmpB.startX,
+                        startY: tmpB.startY,
+                        endX: tmpB.endX,
+                        endY: tmpB.endY,
+                        distance: tDistance,
+                        sectionTime: tTime
+                    }
+                    pathSec.push(tmpE);
+                } else { // 중간 도보
+                    pathSec.push({});
+                }
+            }
+            if(subE.trafficType == 1 || subE.trafficType == 2) { // 대중교통
+                let tmpB = JSON.parse(options.body);
+                if (needWalk == true) { // 이전에 도보 경로가 있었음
+                    tmpB.endX = subE.startX;
+                    tmpB.endY = subE.startY;
+                    options.body = JSON.stringify(tmpB);
+                    const tRes = await fetch(tmapUrl, options);
+                    const tData = await tRes.json();
+                    const tDistance = tData.features[0].properties.totalDistance;
+                    const tTime = calculateTime(tDistance);
+       
+                    pathSec[j-1] = {
+                        trafficType: 3,
+                        startX: tmpB.startX,
+                        startY: tmpB.startY,
+                        endX: tmpB.endX,
+                        endY: tmpB.endY,
+                        distance: tDistance,
+                        sectionTime: tTime
+                    };
+                }
+                
+                var tmpE = {
+                    trafficType: subE.trafficType,
+                    startX: subE.startX,
+                    startY: subE.startY,
+                    endX: subE.endX,
+                    endY: subE.endY,
+                    distance: subE.distance,
+                    sectionTime: subE.sectionTime,
+                    stationCount: subE.stationCount,
+                    lane: subE.lane,
+                    startArsID: subE.startArsID,
+                    endArsID: subE.endArsID,
+                    passStopList: subE.passStopList.stations
+                };
+                pathSec.push(tmpE);
+                tmpB.startX = subE.endX;
+                tmpB.startY = subE.endY;
+                options.body = JSON.stringify(tmpB);
+                needWalk = false;
+            }
+        }
+        const pathSec2 = [];
+        for (let k = 0; k < pathSec.length; k++) {
+            const element = pathSec[k];
+            if(Number.isInteger(element.sectionTime)) {
+                totalTime += element.sectionTime;
+            }
+            var valueArr = Object.values(element);
+            pathSec2.push(valueArr);
+        }
+        pathSec.push(totalTime);
+        pathSec2.push(totalTime);
+        pathArr.push(pathSec);
+        pathArr2.push(pathSec2);
+        
+    }
+    console.log(pathArr);
+    res.json(pathArr2);
+}
+
+function calculateTime(distance) {
+    if (distance > 99 || distance < 1000) {
+        var quotient = Math.floor(distance / 10);
+        var tensDigit = quotient % 10;
+        if (tensDigit > 5)
+            distance = Math.round(distance / 100) * 100;
+    }
+    const speed = 3.5;    // 속도 (km/h)
+
+    // 속도를 분 당 거리로 변환 (m/분)
+    const speedPerMinute = (speed * 1000) / 60;
+  
+    // 거리를 시간으로 나누어 시간 계산 (분)
+    let time = distance / speedPerMinute;
+    if(time < 1)
+        time = 1;
+
+    return Math.round(time);
 }
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const reviewW = require('./review');
 
 
 // listen(서버띄울 포트번호, 띄운 후 실행할 코드) , http://localhost:8080/
-app.listen(8080, function(){
-    console.log('listening on 8080');
+app.listen(8082, function(){
+    console.log('listening on 8082');
 });
 
 //app.get('경로', function(요청, 응답){});
@@ -15,22 +15,36 @@ app.listen(8080, function(){
 //res.json() --> json 보내기
 app.get('/', async (요청, 응답) => {
 
-    var url = 'http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCtyCodeList';
-    var queryParams = '?' + encodeURIComponent('serviceKey') + '=49i%2BWE6UMj7c7HFKZNKH0CBA0Rn4JlJk5%2FJERy0A2rcwSqkYXiockIJL4IBNdzyr%2F0cgmmxa0TA3MNLbomlIjw%3D%3D'; /* Service Key*/
-    queryParams += '&' + encodeURIComponent('_type') + '=' + encodeURIComponent('json'); /* */
+    var request = require('request');
 
-    const testR = await fetch(url + queryParams);
-    const testR2 = await testR.json();
-    // console.dir(testR2.response.body.items, {'maxArrayLength':null});
-    응답.send(testR2.response.body.items);
+    var url = 'http://apis.data.go.kr/B553766/smt-transfer/transfer';
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '=32c8O/2CZv4jgj8cvCaCc7vhw9VOZ6ntxQz77NBxnqUdp1i0fUoCB2sQHZGgY8PusgYc26+GftipAB512U4KJg=='; /* Service Key*/
+    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
+    queryParams += '&' + encodeURIComponent('transferStation') + '=' + encodeURIComponent('0212'); /* */
+    queryParams += '&' + encodeURIComponent('fromLine') + '=' + encodeURIComponent('2'); /* */
+    queryParams += '&' + encodeURIComponent('toLine') + '=' + encodeURIComponent('7'); /* */
+    var respp;
+    var bodyyy;
+    
+    request({
+        url: url + queryParams,
+        method: 'GET',
+    }, function (error, response, body) {
+        console.log('Status', response.statusCode);
+        console.log('Headers', JSON.stringify(response.headers));
+        console.log('Reponse received', body);
+    });
+    응답.send('testR2');
 });
 
 // /search -> 대중교통 검색, /pathfind -> 길찾기
 // const { xxx } = req.params => 이런것도 있다
 app.get('/search/:value', searchT.searchTrans);
-app.get('/pathfind/:value', pathF.searchPlace);
-app.get('/review', reviewW.reviewload);
 app.get('/search/time/:cityCode/:stID', searchT.getTime)
+app.get('/pathfind/sp/:value', pathF.searchPlace);
+app.get('/pathfind/pf/:value', pathF.pathFind);
+app.get('/review', reviewW.reviewload);
 // url 인코딩 https://it-eldorado.tistory.com/143
 // apiKey = 0QNZgti0UA7t0YRwd3T7Qs2pyfFuFAHK6ZrPCSV/KS4
 // 한시간만에 Node.js 백엔드 기초 끝내기 (ft. API 구축) - https://youtu.be/Tt_tKhhhJqY

908b80acbc927a2c7105a59cbffcd827dec54e66
HS-0914 <gudtn2238@gmail.com>
2023-10-16 18:10:36 +0900

    edit1016

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.listen(PORT, () => {
 //app.get('경로', (요청, 응답) => {});
 //res.json() --> json 보내기
 app.get('/', async (요청, 응답) => {
-    응답.send('testR23');
+    응답.send('test text');
 });
 
 // /search -> 대중교통 검색, /pathfind -> 길찾기

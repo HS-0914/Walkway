786f672e3ba7ff19c7dc9530eb81393fbb5e4617
Byun Jin Hwan <ataraxia7899@gmail.com>
2023-10-17 01:13:04 +0900

    로그인기능

--- login.js
+++ login.js
@@ -1,3 +1,52 @@
-export async function login(req, res) {
-    
+import db from './db.js';
+
+async function login(req, res) {
+
+    const { login_id } = req.params;
+    const { password } = req.params;
+    const con = await db.con;
+
+    // 로그인 라우트
+    try {
+        const [rows] = await con.execute('SELECT * FROM User WHERE login_id = ? AND password = ?', [login_id, password]);
+
+        if (rows.length > 0) {
+            res.json({ message: '로그인 성공!', user: rows[0] });
+        } else {
+            res.status(401).json({ message: '로그인 실패. 유효하지 않은 자격 증명입니다.' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: '서버 에러' });
+    }
+
+    res.send("loginnnnnn");
 }
+
+async function signup(req, res) {
+    const { login_value } = req.params;
+    const login_Arrangement = JSON.parse(login_value); //문자열화된 배열 -> 배열로 바꿔줌.
+    const con = await db.con;
+
+    try {
+        // 사용자 중복 확인
+        const [existingUsers] = await con.execute('SELECT * FROM User WHERE login_id = ?', [login_Arrangement[0]]);
+
+        if (existingUsers.length > 0) {
+            return res.status(400).json({ message: '이미 존재하는 사용자입니다.' });
+        }
+
+        // 새로운 사용자 추가
+        const [result] = await con.execute('INSERT INTO User (login_id, password, name, phone_number) VALUES (?, ?, ?, ?)', login_Arrangement);
+
+        if (result.affectedRows === 1) {
+            return res.json({ message: '회원가입 성공!' });
+        } else {
+            return res.status(500).json({ message: '회원가입 실패. 서버 에러.' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).json({ message: '서버 에러' });
+    }
+};
+export default { login , signup };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import login from './login.js';
+import logins from './login.js';
 import searchT from './search.js';
 import pathF from './pathfind.js';
 import pathF2 from './pf2.js';
@@ -29,6 +29,8 @@ app.get('/search2/:value', searchT.searchTrans2); // 대중교통 검색
 app.get('/search/bustime/:stID', searchT.busgetTime) // 버스 도착정보
 app.get('/search/metrotime/:stName', searchT.metrogetTime) // 지하철 도착정보
 app.get('/pathfind/sp/:value', pathF.searchPlace);
+app.get('/login/:login_id/:password', logins.login); // 로그인
+app.get('/signup/:login_value', logins.signup); // 회원가입
 // app.get('/pathfind/pf/:value', pathF.pathFind);
 app.get('/pathfind/pf/:value', pathF2.pathFind);
 // app.get('/pathfind/pd/:value', pathF.pathDraw);

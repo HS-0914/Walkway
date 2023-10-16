0d100ed0f065ccfd6f302fc464d620440da6cb3e
Byun Jin Hwan <ataraxia7899@gmail.com>
2023-10-17 02:20:20 +0900

    json값수정

--- login.js
+++ login.js
@@ -40,13 +40,13 @@ async function signup(req, res) {
         const [result] = await con.execute('INSERT INTO User (login_id, password, name, phone_number) VALUES (?, ?, ?, ?)', login_Arrangement);
 
         if (result.affectedRows === 1) {
-            return res.json({ message: '회원가입 성공!' });
+            return res.json('회원가입 성공!');
         } else {
-            return res.status(500).json({ message: '회원가입 실패. 서버 에러.' });
+            return res.status(500).json('회원가입 실패. 서버 에러.');
         }
     } catch (error) {
         console.error('Error:', error);
-        return res.status(500).json({ message: '서버 에러' });
+        return res.status(500).json('서버 에러');
     }
 };
 export default { login , signup };

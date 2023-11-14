df99ff7b8b55ebf2ddce553f02e4a4553054c088
HS-0914 <gudtn2238@gmail.com>
2023-11-14 18:22:55 +0900

    1114

--- alarm.js
+++ alarm.js
@@ -40,10 +40,57 @@ async function schedule(req, res) {
     res.json(sendArr);
 }
 
+/*
+{
+  '오전 09:00': [ '월', '화' ],
+  '정류장 - 00분 00초 _ bus.png': [ [ 127, 36 ], [ '마을:568', '마을:564' ] ],
+  fire: 1,
+  type: 'bus'
+}
+*/
 async function schedule2(req, res) {
     const val = req.body;
-    console.log(val);
-    res.json("test");
+    const keyInd = Object.keys(val);
+    const sendArr = [];
+
+    if (val.type == 'bus') {
+
+        let url = `https://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList?serviceKey=${publicKey}&_type=json&gpsLati=${val.keyInd[1][0][1]}&gpsLong=${val.keyInd[1][0][0]}`;
+        const locSch = await fetch(url);
+        const locRes = await locSch.json();
+        const locData = locRes.response.body.items.item[0];
+    
+        url = `https://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList?serviceKey=${publicKey}&_type=json&cityCode=${locData.citycode}&nodeId=${locData.nodeid}`;
+        const timeSch = await fetch(url);
+        const timeRes = await timeSch.json();
+        let timeData;
+        if (timeRes.response.body.totalCount > 0) {
+            timeData = timeRes.response.body.items.item;
+        }
+        else {
+            timeData = false;
+        }
+        if(timeData) {
+            console.log(timeData);
+            for (let i = 0; i < timeData.length; i++) {
+                const element = Object.values(timeData[i]);
+                const tmpArr = [element[5], Math.ceil(element[1]/60), element[0]]; // 버스 번호, 예상 시간(초),  남은 정류장 수
+          
+                sendArr.push(tmpArr);
+            }
+        }
+        else {
+            sendArr.push(-1);
+        }
+        // sendArr.sort(function(a, b) {
+        //     return a[1] - b[1];
+        // });
+        sendArr.sort((a, b) => a[1] - b[1]);
+    } else {
+        
+    }
+    console.log(sendArr);
+    res.json(sendArr);
 }
 
 export default { schedule, schedule2 };

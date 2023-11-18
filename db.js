3e81a2d88abcecd72492180bfa3732adbe534678
HS-0914 <gudtn2238@gmail.com>
2023-11-19 01:52:52 +0900

    db변경

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise'; // db연동
 
 var con = mysql.createConnection({
-    host: "34.64.230.27",
+    host: "34.64.83.119",
     user: "root",
     password: "0000",
     database : 'walkway'
